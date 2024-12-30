dd34d3a13c8b36925bdbd9b10d49a1bfce808645
Amin Marheraoui <95435963+aminmarh@users.noreply.github.com>
2024-12-30 15:45:06 +0100

    feat: add routes to be able to add, delete or view the routes po

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,11 @@
 from app.routes.auth_routes import auth_bp
 from app.routes.weather_routes import weather_bp
 from app.routes.favorite_routes import favorite_bp
+from app.routes.threshold_routes import threshold_bp
 
 
 def init_routes(app):
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(weather_bp, url_prefix="/api/weather")
     app.register_blueprint(favorite_bp, url_prefix="/api/favorites")
+    app.register_blueprint(threshold_bp, url_prefix="/api/thresholds")
---
+++ app/routes/threshold_routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+from app.controllers.threshold_controller import (
+    create_storm_threshold,
+    create_flood_threshold,
+    create_heatwave_threshold,
+    delete_threshold,
+    get_thresholds,
+)
+
+threshold_bp = Blueprint("threshold", __name__)
+
+threshold_bp.add_url_rule("/storm", view_func=create_storm_threshold, methods=["POST"])
+threshold_bp.add_url_rule("/flood", view_func=create_flood_threshold, methods=["POST"])
+threshold_bp.add_url_rule(
+    "/heatwave", view_func=create_heatwave_threshold, methods=["POST"]
+)
+threshold_bp.add_url_rule("/delete", view_func=delete_threshold, methods=["DELETE"])
+threshold_bp.add_url_rule("", view_func=get_thresholds, methods=["GET"])
