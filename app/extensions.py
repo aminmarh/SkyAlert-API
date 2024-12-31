bb79627d506752c6c86ea0ba661f0b163f7a7f46
Amin Marheraoui <95435963+aminmarh@users.noreply.github.com>
2024-12-31 18:24:21 +0100

    refactor: store invalid tokens in database instead of a Python list for scalability

--- app/extensions.py
+++ app/extensions.py
@@ -7,9 +7,17 @@ jwt = JWTManager()
 migrate = Migrate()
 
 
+def add_token_to_blacklist(jti, expires_at):
+    from app.models.user import TokenBlocklist
+
+    token = TokenBlocklist(jti=jti, expires_at=expires_at)
+    db.session.add(token)
+    db.session.commit()
+
+
 @jwt.token_in_blocklist_loader
-def check_if_token_in_blacklist(jwt_header, jwt_payload):
-    from app.controllers.auth_controller import blacklist_tokens
+def check_if_token_in_blocklist(jwt_header, jwt_payload):
+    from app.models.user import TokenBlocklist
 
     jti = jwt_payload["jti"]
-    return jti in blacklist_tokens
+    return TokenBlocklist.query.filter_by(jti=jti).first() is not None
--- app/models/user.py
+++ app/models/user.py
@@ -104,3 +104,14 @@ class FloodThreshold(db.Model):
     )
     precipitation = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
+class TokenBlocklist(db.Model):
+    __tablename__ = "token_blocklist"
+
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(36), nullable=False, unique=True)
+    created_at = db.Column(
+        db.DateTime, default=datetime.datetime.utcnow, nullable=False
+    )
+    expires_at = db.Column(db.DateTime, nullable=False)
