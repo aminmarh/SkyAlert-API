cb3bcb60dbe559ea89f2e9c6c62993659a02e93e
Amin Marheraoui <95435963+aminmarh@users.noreply.github.com>
2024-12-31 22:12:56 +0100

    refactor: add type annotations to auth_schema

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,20 +1,20 @@
 from marshmallow import Schema, fields, validate
 
 
-class RegisterSchema(Schema):
-    username = fields.Str(
-        required=True,
-        validate=validate.Regexp(
+class UsernameType(fields.Str):
+    def __init__(self, *args, **kwargs):
+        kwargs["validate"] = validate.Regexp(
             r"^[a-zA-Z\s]+$",
             error="Username must be a string containing only letters and spaces",
-        ),
-        description="Username of the user",
-    )
-    email = fields.Email(required=True, description="Email address of the user")
-    password = fields.Str(
-        required=True,
-        validate=[
-            validate.Length(min=6),
+        )
+        kwargs["description"] = "Username of the user"
+        super().__init__(*args, **kwargs)
+
+
+class PasswordType(fields.Str):
+    def __init__(self, *args, **kwargs):
+        kwargs["validate"] = [
+            validate.Length(min=6, error="Password must be at least 6 characters long"),
             validate.Regexp(
                 r"^[^\s"
                 r"\U0001F600-\U0001F64F"
@@ -26,125 +26,60 @@ class RegisterSchema(Schema):
                 r"\U0001F900-\U0001F9FF"
                 r"\U0001FA00-\U0001FA6F"
                 r"\U0001FA70-\U0001FAFF]+$",
-                error="Password must not contain spaces",
+                error="Password must not contain spaces or emojis",
             ),
-        ],
-        description="Password of the user",
-    )
+        ]
+        kwargs["description"] = "Password of the user"
+        super().__init__(*args, **kwargs)
+
+
+class EmailType(fields.Email):
+    def __init__(self, *args, **kwargs):
+        kwargs["description"] = "Email address of the user"
+        super().__init__(*args, **kwargs)
+
+
+class CodeType(fields.Str):
+    def __init__(self, *args, **kwargs):
+        kwargs["validate"] = [
+            validate.Length(min=6, max=6, error="Code must be 6 digits long"),
+            validate.Regexp(r"^\d+$", error="Code must contain only digits"),
+        ]
+        kwargs["description"] = "Verification code sent to the user"
+        super().__init__(*args, **kwargs)
+
+
+class RegisterSchema(Schema):
+    username = UsernameType(required=True)
+    email = EmailType(required=True)
+    password = PasswordType(required=True)
 
 
 class LoginSchema(Schema):
-    email = fields.Email(required=True, description="Email address used for login")
-    password = fields.Str(
-        required=True,
-        validate=[
-            validate.Length(min=6),
-            validate.Regexp(
-                r"^[^\s"
-                r"\U0001F600-\U0001F64F"
-                r"\U0001F300-\U0001F5FF"
-                r"\U0001F680-\U0001F6FF"
-                r"\U0001F700-\U0001F77F"
-                r"\U0001F780-\U0001F7FF"
-                r"\U0001F800-\U0001F8FF"
-                r"\U0001F900-\U0001F9FF"
-                r"\U0001FA00-\U0001FA6F"
-                r"\U0001FA70-\U0001FAFF]+$",
-                error="Password must not contain spaces",
-            ),
-        ],
-        description="Password used for login",
-    )
+    email = EmailType(required=True)
+    password = PasswordType(required=True)
 
 
 class UserUpdateSchema(Schema):
-    username = fields.Str(
-        validate=validate.Regexp(
-            r"^[a-zA-Z\s]+$",
-            error="Username must be a string containing only letters and spaces",
-        ),
-        description="Updated username",
-    )
-    email = fields.Email(description="Updated email address")
+    username = UsernameType(required=False)
+    email = EmailType(required=False)
 
 
 class PasswordUpdateSchema(Schema):
-    current_password = fields.Str(
-        required=True,
-        validate=[
-            validate.Length(min=6),
-            validate.Regexp(
-                r"^[^\s"
-                r"\U0001F600-\U0001F64F"
-                r"\U0001F300-\U0001F5FF"
-                r"\U0001F680-\U0001F6FF"
-                r"\U0001F700-\U0001F77F"
-                r"\U0001F780-\U0001F7FF"
-                r"\U0001F800-\U0001F8FF"
-                r"\U0001F900-\U0001F9FF"
-                r"\U0001FA00-\U0001FA6F"
-                r"\U0001FA70-\U0001FAFF]+$",
-                error="Password must not contain spaces",
-            ),
-        ],
-        description="Current password of the user",
-    )
-    new_password = fields.Str(
-        required=True,
-        validate=[
-            validate.Length(min=6),
-            validate.Regexp(
-                r"^[^\s"
-                r"\U0001F600-\U0001F64F"
-                r"\U0001F300-\U0001F5FF"
-                r"\U0001F680-\U0001F6FF"
-                r"\U0001F700-\U0001F77F"
-                r"\U0001F780-\U0001F7FF"
-                r"\U0001F800-\U0001F8FF"
-                r"\U0001F900-\U0001F9FF"
-                r"\U0001FA00-\U0001FA6F"
-                r"\U0001FA70-\U0001FAFF]+$",
-                error="Password must not contain spaces",
-            ),
-        ],
-        description="New password of the user",
-    )
+    current_password = PasswordType(required=True)
+    new_password = PasswordType(required=True)
 
 
 class RequestPasswordResetSchema(Schema):
-    email = fields.Email(required=True, description="Email address of the user")
+    email = EmailType(required=True)
 
 
 class VerifyResetCodeSchema(Schema):
-    email = fields.Email(required=True, description="Email address of the user")
-    code = fields.Str(
-        required=True,
-        validate=[
-            validate.Length(min=6, max=6),
-            validate.Regexp(
-                r"^\d+$", error="Code must contain only digits and no spaces"
-            ),
-        ],
-        description="Code sent to the user's email",
-    )
+    email = EmailType(required=True)
+    code = CodeType(required=True)
 
 
 class PasswordForgotSchema(Schema):
-    email = fields.Email(required=True, description="Email address of the user")
-    code = fields.Str(
-        required=True,
-        validate=[
-            validate.Length(min=6, max=6),
-            validate.Regexp(
-                r"^\d+$", error="Code must contain only digits and no spaces"
-            ),
-        ],
-        description="Code sent to the user's email",
-    )
-    new_password = fields.Str(
-        required=True,
-        validate=[
-            validate.Length(min=6),
-        ],
-        description="New password of the user",
-    )
+    email = EmailType(required=True)
+    code = CodeType(required=True)
+    new_password = PasswordType(required=True)
