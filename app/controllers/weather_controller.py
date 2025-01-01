941969de3956c4ab856e0df15427090d7f25d0b3
Amin Marheraoui <95435963+aminmarh@users.noreply.github.com>
2025-01-01 20:09:20 +0100

    feat: add controller

--- app/controllers/weather_controller.py
+++ app/controllers/weather_controller.py
@@ -4,7 +4,7 @@ from marshmallow.exceptions import ValidationError
 
 from app.helpers.weather import WeatherAPI
 from app.models.user import User
-from app.schemas.weather_schema import WeatherRequestSchema
+from app.schemas.weather_schema import WeatherRequestSchema, SearchLocationSchema
 
 
 @jwt_required()
@@ -46,10 +46,19 @@ def get_forecast():
               properties:
                 current:
                   type: array
+                  items:
+                    type: object
+                  description: List of current weather data
                 forecast:
                   type: array
+                  items:
+                    type: object
+                  description: List of forecast data
                 location:
                   type: array
+                  items:
+                    type: object
+                  description: List of location data
             message:
               type: string
               example: Weather forecast for Paris retrieved successfully
@@ -97,6 +106,101 @@ def get_forecast():
                 "status": "success",
                 "data": forecast,
                 "message": f"Weather forecast for {city} retrieved successfully",
+            }
+        ),
+        200,
+    )
+
+
+@jwt_required()
+def search_location():
+    """
+    Recherche de localisation.
+    ---
+    tags:
+      - Weather
+    security:
+      - Bearer: []
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            query:
+              type: string
+              description: Name of the city
+              default: Paris
+              example: Paris
+    responses:
+      200:
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+              example: success
+            data:
+              type: object
+              properties:
+                name:
+                  type: string
+                  example: Paris
+                region:
+                  type: string
+                  example: Ile-de-France
+                country:
+                  type: string
+                  example: France
+                lat:
+                  type: string
+                  example: 48.85
+                lon:
+                  type: string
+                  example: 2.35
+                url:
+                  type: string
+                  example: paris-ile-de-france-france
+            message:
+              type: string
+              example: Weather location search for Paris retrieved successfully
+      400:
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+              example: "error"
+            data:
+              type: object
+              example: null
+            message:
+              type: string
+              example: "Validation failed"
+    """
+    data = request.json
+
+    try:
+        validated_data = SearchLocationSchema().load(data)
+    except ValidationError as err:
+        return {
+            "status": "error",
+            "data": None,
+            "message": "Validation failed",
+            "errors": err.messages,
+        }, 400
+
+    query = validated_data["query"]
+
+    city = WeatherAPI.location_search(query)
+
+    return (
+        jsonify(
+            {
+                "status": "success",
+                "data": city,
+                "message": f"Weather location search for {query} retrieved successfully",
             }
         ),
         200,
