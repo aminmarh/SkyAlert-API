2a80d0af45981feaade78b98fceee020d22b1b4e
Amin Marheraoui <95435963+aminmarh@users.noreply.github.com>
2025-01-01 20:09:01 +0100

    feat: add rooutes

--- app/routes/weather_routes.py
+++ app/routes/weather_routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from app.controllers.weather_controller import (
     get_forecast,
+    search_location,
 )
 
 weather_bp = Blueprint("weather", __name__)
 
 weather_bp.add_url_rule("/forecast", view_func=get_forecast, methods=["POST"])
+weather_bp.add_url_rule("/search", view_func=search_location, methods=["POST"])
